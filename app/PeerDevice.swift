3a0743d86684c5f8c4654624e3fc2de248c7e1e0
JJ <jj@Janardhans-MacBook-Pro.local>
2026-03-21 21:55:47 -0500

    Added - PeerDevice.swift

---
+++ app/PeerDevice.swift
@@ -0,0 +1,14 @@
+//
+//  PeerDevice.swift
+//  App
+//
+//  Created by Janardhan on 2026-03-21.
+//
+
+
+struct PeerDevice: Identifiable {
+    let id: String
+    let name: String
+    let systemImage: String
+    let status: String
+}
